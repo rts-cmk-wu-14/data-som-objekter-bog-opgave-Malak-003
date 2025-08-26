2c25cf1c611c57353729dbf59c9599bda1458bcd
= <=>
2025-08-26 12:10:33 +0200

    Opgave 1 løst

---
+++ script.js
@@ -0,0 +1,16 @@
+// opgave 1
+
+const person = {
+  firstName: "Lena",
+  lastName: "Hansen",
+  age: 35,
+  city: "Odense",
+  pets: ["kat"]
+}
+
+console.log(person.city)
+console.log(person.age)
+console.log(person.pets)
+
+console.log(person['city'])
+console.log(person['pets'])
