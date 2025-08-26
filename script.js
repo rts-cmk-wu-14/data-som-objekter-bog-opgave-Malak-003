252ac4bf99a8550a298a28356800cd0666bbdae8
= <=>
2025-08-26 12:52:30 +0200

    opgave 2 løst

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ const person = {
   lastName: "Hansen",
   age: 35,
   city: "Odense",
-  pets: ["kat"]
+  pet: ["kat"]
 }
 
 console.log(person.city)
 console.log(person.age)
-console.log(person.pets)
+console.log(person.pet)
 
 console.log(person['city'])
-console.log(person['pets'])
+console.log(person['pet'])
+
+// Opgave 2 
+const personT = {
+  firstName: "Lena",
+  lastName: "Hansen",
+  married: true,
+  age: 35,
+  hobbies: ["music, painting, cooking"],
+  city: "Odense",
+  pet:{
+    type:"kat",
+    name:"luke"
+  } 
+}
+
+personT.hobbies.forEach(function(hobby){
+    console.log(hobby)
+})
+
+console.log(personT.pet.type)
+console.log(personT.pet.name)
